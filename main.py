cde7ed1dd8c934bffdde20a5ed4da3e9ad118012
kabuto arai <kabutoarai@kabutonoiMac.local>
2024-01-23 20:47:18 +0900

    to_MBP

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 
 # from scipy.special import fresnel
 
+######################
+# # DFT行列の注意点
+# 時間信号から周波数信号に変換するときは、fの成分が見たいから、exp(-j2pi*f*t)を内積する。
+# 行列表現で
+# x(f) = D^H x(t) と表現する場合、Dの中身はプラスの符号（exp(+j2pi*f*t)）で作成しておく必要。
+# Numpyで以下のコードでDFT行列作成したとき、各要素の位相はマイナス（exp(-j2pi*f*t)）になっているので、注意（この場合 x(f) = D x(t) でオッケー（共役転置不要））
+# D = np.fft.fft(np.eye(N)) / np.sqrt(N)  # DFT
+######################
+
 
 def main():
     N = 200
@@ -14,18 +23,36 @@ def main():
     yn = np.arange(N) * d - (N - 1) * d / 2
 
     # target
-    AoA = 45 * np.pi / 180
+    AoA = 20 * np.pi / 180
     r = 1 / wl
     nu = np.pi * d ** 2 * np.cos(AoA) ** 2 / r
     omega = 2 * np.pi * d * np.sin(AoA)
     gamma = omega + (N - 1) * nu
 
+    # Est
+
+    HPW_ULA = np.arcsin((2.783 / N) / np.pi) * 180/np.pi
+
+    AoA_error = 0.1 * np.pi / 180
+    r_error = 0.1 / wl
+    AoA_est = AoA + AoA_error
+    r_est = r + r_error
+    nu_est = np.pi * d ** 2 * np.cos(AoA_est) ** 2 / r_est
+    omega_est = 2 * np.pi * d * np.sin(AoA_est)
+    gamma_est = omega_est + (N - 1) * nu_est
+
     ###############
     G_array = np.zeros(N, dtype=np.float64)
+    G_array_est = np.zeros(N, dtype=np.float64)
+    G_diff_array = np.zeros(N, dtype=np.float64)
     for n in range(N):
         omega_n = 2 * np.pi * n / N
         G = calc_fresnel_integral(a=nu, b=omega_n - gamma, c=0, N=N)
+        G_est = calc_fresnel_integral(a=nu_est, b=omega_n - gamma_est, c=0, N=N)
         G_array[n] = np.abs(G)
+        G_array_est[n] = np.abs(G_est)
+        G_diff_array[n] = np.abs(G - G_est)
+        # G_diff_array[n] = np.abs(G) - np.abs(G_est)
     # plt.plot(G_array, "x-")
     # plt.show()
     ###############
@@ -38,12 +65,15 @@ def main():
     eee = np.abs(a_vec_fr - a_vec_fr_2)
 
     D = np.fft.fft(np.eye(N)) / np.sqrt(N)  # DFT
+    D = np.conj(D)
     Da = np.conj(D) @ a_vec
     Da_fr = np.conj(D) @ a_vec_fr
 
-    plt.plot(np.abs(Da), label="Da")
-    plt.plot(np.abs(Da_fr), label="Da_fr")
-    plt.plot(G_array, "x-", label="Fresnel integral approx.")
+    # plt.plot(np.abs(Da), label="Da")
+    # plt.plot(np.abs(Da_fr), "x--", label="Da_fr")
+    plt.plot(G_array, "-", label="G (Fresnel approx.)")
+    plt.plot(G_array_est, "-", label="G_est (Fresnel approx.)")
+    plt.plot(G_diff_array, "-", label="G_diff (Fresnel approx.)")
     plt.legend()
     plt.show()
 
